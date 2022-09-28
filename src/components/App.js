9ff7641665eac031719d14bee07057d2a818ac3a
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2022-09-28 19:38:39 +0300

    created web version

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import logo from './assets/logo.svg';
+import star from './assets/star.svg';
 
 import data from './data/stays.json';
 import { useEffect } from 'react';
@@ -43,7 +44,32 @@ function App() {
           <div className="main__top">
             <h1 className="main__title">Stays in Finland</h1>
 
-            <p className='main__count'>{chanedRooms.length}+ stays</p>
+            <p className="main__count">{chanedRooms.length}+ stays</p>
+          </div>
+
+          <div className="main__bottom">
+            {chanedRooms.map((room) => (
+              <div className="room">
+                <div className="room__container-photo">
+                  <img src={room.photo} alt="" className="room__photo" />
+                </div>
+
+                <div className="room__middle">
+                  <span className="room__text">
+                    {room.superHost && <span className="room__super-host">Super host</span>}
+                    {room.type} {room.beds > 1 ? ` . ${room.beds} beds` : ``}
+                  </span>
+
+                  <div className="room__rating">
+                    <img src={star} alt="" className="room__star" />
+
+                    <p className="room__text">{room.rating}</p>
+                  </div>
+                </div>
+
+                <p className="room__describe">{room.title}</p>
+              </div>
+            ))}
           </div>
         </main>
       </div>
